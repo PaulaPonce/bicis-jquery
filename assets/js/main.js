407405c5700b722141d11796b9413a0dfd6460fa
Paula Ponce <paula.poncez@usach.cl>
2017-07-01 14:28:17 -0400

    lastname validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,15 @@ $(document).ready(function(){
 	function validateForm(event){
 		event.preventDefault();
 		var name = $("#name").val();
+		var lastName = $("#lastname").val();
+
 			//validación nombre
-			if(!/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]{2,11})+$/.test(name)){
-				$(".name-container").append("<span>Debe ingresar su nombre (debe comenzar con mayúscula)</span>")
+			if(!/^([a-zñáéíóú]{1,11})+$/.test(name)){
+				$(".name-container").append("<span>Debe ingresar su nombre (solo caracteres de la A a la Z)</span>");
+			}
+			//validación apellido
+			if(!/^([a-zñáéíóú]{1,11})+$/.test(lastName)){
+				$(".lastname-container").append("<span>Debe ingresar su apellido (solo caracteres de la A a la Z)</span>");
 			}
 	}
 });
