913a1e74ce6237a837d3073f3e84a6cb29443ff6
Paula Ponce <paula.poncez@usach.cl>
2017-07-01 14:19:38 -0400

    name validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
-function validateForm(){
-	/* Escribe tú código aquí */
-}
+$(document).ready(function(){
+	$("button").on("click", validateForm);
+
+	function validateForm(event){
+		event.preventDefault();
+		var name = $("#name").val();
+			//validación nombre
+			if(!/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]{2,11})+$/.test(name)){
+				$(".name-container").append("<span>Debe ingresar su nombre (debe comenzar con mayúscula)</span>")
+			}
+	}
+});
+
