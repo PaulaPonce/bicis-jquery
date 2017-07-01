8c2fae124b7b6e57ae291df08feb1fcdfb02f413
Paula Ponce <paula.poncez@usach.cl>
2017-07-01 15:01:21 -0400

    select validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,23 +7,28 @@ $(document).ready(function(){
 		var lastName = $("#lastname").val();
 		var email = $("#input-email").val();
 		var pass = $("#input-password").val();
+		var bike = $("select");
 
-			//validación nombre
-			if(!/^([a-zñáéíóú]{1,11})+$/.test(name)){
-				$(".name-container").append("<span>Debe ingresar su nombre (solo caracteres de la A a la Z)</span>");
-			}
-			//validación apellido
-			if(!/^([a-zñáéíóú]{1,11})+$/.test(lastName)){
-				$(".lastname-container").append("<span>Debe ingresar su apellido (solo caracteres de la A a la Z)</span>");
-			}
-			//validación correo
-			if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
-				$(".email-container").append("<span>Verifique su email</span>");
-			}
-			//validación contraseña
-			if(pass.length <= 6 || pass === "123456" || pass === "098765" || pass === "password"){
-				$(".password-container").append("<span>La contraseña debe tener al menos 6 caracteres</span>");
-			}	
+		//validación nombre
+		if(!/^([a-zñáéíóú]{1,11})+$/.test(name)){
+			$(".name-container").append("<span>Debe ingresar su nombre (solo caracteres de la A a la Z)</span>");
+		}
+		//validación apellido
+		if(!/^([a-zñáéíóú]{1,11})+$/.test(lastName)){
+			$(".lastname-container").append("<span>Debe ingresar su apellido (solo caracteres de la A a la Z)</span>");
+		}
+		//validación correo
+		if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+			$(".email-container").append("<span>Verifique su email</span>");
+		}
+		//validación contraseña
+		if(pass.length <= 6 || pass === "123456" || pass === "098765" || pass === "password"){
+			$(".password-container").append("<span>La contraseña debe tener al menos 6 caracteres</span>");
+		}
+		//validación tipo de bici
+		if(bike[0].selectedIndex == 0){
+		$(".bike-container").append("<span>Debes seleccionar al menos un tipo de bici</span>");
+		}
 	}
 });
 
